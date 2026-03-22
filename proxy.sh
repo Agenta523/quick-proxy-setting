359349507d1046e07506c57f137d77e998b7bbc1
Agena <163381126+Agenta523@users.noreply.github.com>
2026-03-23 00:15:13 +0900

    Add proxy management script in proxy.sh

---
+++ proxy.sh
@@ -0,0 +1,17 @@
+proxy = "http://cproxy.okinawa-ct.ac.jp:8080"
+proxy () {
+  case "$1" in
+    on)
+      export http_proxy=$proxy
+      export https_proxy=$proxy
+      echo "proxy enabled"
+      ;;
+    off)
+      unset http_proxy https_proxy
+      ;;
+    status)
+      echo "http_proxy=$http_proxy"
+      echo "https_proxy=$https_proxy"
+      ;;
+  esac
+}
